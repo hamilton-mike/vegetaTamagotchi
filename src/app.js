95386d6348f3c7b87deb14ef6e3aef1da31eefae
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-11-08 00:21:32 -0700

    working timing for sounds and have btn clicks work and not work when need to, besides so css that needs to be added almost done for my liking

--- src/app.js
+++ src/app.js
@@ -1,52 +1,41 @@
 // --- DOM Elements ---
-const hungryPTag = document.querySelector('#hungry');
-
+const videoDiv = document.querySelector('.yt');
+const iframe = document.querySelector('.giphy iframe');
+const span = document.querySelector("#enemy");
 const workoutProgressBar = document.querySelector('#workout');
-
-
-const happyPTag = document.querySelector('#happy');
-const agePTag = document.querySelector('#age');
-const vegeta = document.querySelector('#vegeta');
-const videoDiv = document.querySelector('#videoDiv');
-const iframe = document.querySelector('.extras iframe');
-
-const speakBtn = document.querySelector('#speak');
-const attackBtn = document.querySelector('#fight');
-const eatBtn = document.querySelector('#eat');
-
+const happyProgressBar = document.querySelector('#happy');
+const prideProgressBar = document.querySelector('#pride');
 
 const workoutBtn = document.querySelector('.workout-btn');
 const happyBtn = document.querySelector('.happy-btn')
 const prideBtn = document.querySelector('.pride-btn')
+const attackBtn = document.querySelector('.big-bang-attack');
 
-console.log(workoutBtn);
-console.log(prideBtn);
+function clickFunction() {
+    workoutBtn.addEventListener('click', trianVegeta);
+    workoutBtn.addEventListener('click', checkStatus);
+    happyBtn.addEventListener('click', happyVegeta);
+    happyBtn.addEventListener('click', checkStatus);
+    prideBtn.addEventListener('click', feedVegeta);
+    prideBtn.addEventListener('click', checkStatus);
+}
+clickFunction()
 
-workoutBtn.addEventListener('click', trianVegeta);
-
-// console.log('max', testProgressBar.max);
-// console.log('value', testProgressBar.value);
-// console.log('btn', testBtn);
-// testBtn.addEventListener('click', testVegeta);
-
-// btn -> function -> class method
-
-// --- Images ---
-const stages = ['./img/kid_vegeta.jpeg', './img/vegeta2.jpeg', './img/vegeta_blue.jpeg', './img/sleepingVegeta.jpeg'];
+const enemies = ['Saibamen', 'Andriod 19', 'Goku'];
 // --- Videos ---
 const videos = {
-    kid: '<iframe width="560" height="315" src="https://www.youtube.com/embed/gBE8xj1ISIc?playlist=gBE8xj1ISIc&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay" allowfullscreen></iframe >',
-    adult: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yKYP_EQ_liA?playlist=yKYP_EQ_liA&autoplay=1&loop=1&mute=1&end=216" frameborder="0" allow="autoplay" allowfullscreen></iframe>',
-    god: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oV00HVtdX9w?playlist=oV00HVtdX9w&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>'
+    saibamen: '<iframe width="560" height="315" src="https://www.youtube.com/embed/gBE8xj1ISIc?playlist=gBE8xj1ISIc&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay" allowfullscreen></iframe >',
+    andriod19: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yKYP_EQ_liA?playlist=yKYP_EQ_liA&autoplay=1&loop=1&mute=1&end=216" frameborder="0" allow="autoplay" allowfullscreen></iframe>',
+    goku: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oV00HVtdX9w?playlist=oV00HVtdX9w&autoplay=1&loop=1&mute=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>'
 };
 // --- Gifs ---
 const giphy = {
-    kid: 'https://giphy.com/embed/rGmanVvUtu0og',
-    adult: 'https://giphy.com/embed/V3pgCWBZmBogW5Hroj',
-    god: 'https://giphy.com/embed/ObWhFPAlovcqs'
+    bba: 'https://giphy.com/embed/jaluhXZOhVNmw',
+    default: 'https://giphy.com/embed/zO5p2acZUVWVO'
 };
+
 // --- Sounds ---
-const soundBoards = [
+let soundBoards = [
     'GREATEST WARRIOR - AUDIO FROM JAYUZUMI.COM.mp3',
     "YOU'RE WORTHLESS - AUDIO FROM JAYUZUMI.COM.mp3",
     "STOP IN AND FINISH THIS MYSELF - AUDIO FROM JAYUZUMI.COM.mp3",
@@ -56,78 +45,85 @@ const soundBoards = [
 // --- Global Variables ---
 let randomNum;
 let audio;
-let time = 14000;
+let time = 100000;
 // --- Vegeta Class ---
 class Vegeta {
-    constructor(hunger, happy, workingOut, age, isSaiyan) {
-        this.hunger = hunger;
+    constructor(pride, happy, workingOut, isSaiyan, isGod ) {
+        this.pride = pride;
         this.happy = happy;
-        this.workingOut = workingOut
-        this.age = age;
+        this.workingOut = workingOut;
         this.isSaiyan = false;
+        this.isGod = false;
     }
     default() {
-        testProgressBar.value = this.test;
-        vegeta.src = stages[0];
-        videoDiv.innerHTML = videos["kid"];
-        iframe.src = giphy["kid"];
+        span.innerHTML = enemies[0]
+        videoDiv.innerHTML = videos["saibamen"];
+        iframe.src = giphy["default"];
     }
-    // aging() {
-    //     agePTag.innerHTML = `Age: ${this.age += 1}`
-    // }
-    // eat() {
-    //     hungryPTag.innerHTML = `Hunger: - ${this.hunger += 1}`;
-    // }
-    // happyPlueOne() {
-    //     happyPTag.innerHTML = `Happy: ${this.happy += 1}`;
-    // }
+    attack() {
+        iframe.src = giphy["bba"];
+        bigBangAttack()
+        setTimeout(() => {
+            iframe.src = giphy["default"]
+        }, 8000)
+    }
+    sayianPride() {
+        this.pride++
+        prideProgressBar.value = this.pride;
+    }
+    happyPlueOne() {
+        this.happy++;
+        happyProgressBar.value = this.happy;
+    }
     workout() {
-        console.log('before', this.workingOut, workoutProgressBar.value);
         this.workingOut++;
         workoutProgressBar.value = this.workingOut;
-        console.log('after', this.workingOut, workoutProgressBar.value);
-
-    }
-    thisIsAtest() {
-        // console.log(testProgressBar.value);
-        console.log('before', this.test, testProgressBar.value);
-        this.test++;
-        testProgressBar.value = this.test;
-        console.log('after', this.test, testProgressBar.value);
     }
     adultVegeta() {
-        time = time / 2;
-        hungryPTag.innerHTML = `Hunger: - ${this.hunger = 2}`;
-        happyPTag.innerHTML = `Happy: ${this.happy = 2}`;
-        workoutPTag.innerHTML = `Workout: ${this.workingOut = 2}`;
-        agePTag.innerHTML = `Age: ${this.age = 29}`;
-        iframe.src = giphy["adult"];
-        vegeta.src = stages[1];
-        videoDiv.innerHTML = videos["adult"];
+        span.innerHTML = enemies[1]
+
+        videoDiv.innerHTML = videos["andriod19"];
+
+        this.workingOut = 1;
+        this.happy = 1;
+        this.pride = 1;
+
+        workoutProgressBar.value = this.workingOut;
+        happyProgressBar.value = this.happy;
+        prideProgressBar.value = this.pride;
+
         this.isSaiyan = true;
+        time = 10000;
     }
     godVegeta() {
-        clearInterval(intervalId);
-        hungryPTag.innerHTML = ``;
-        happyPTag.innerHTML = `Congrats you made Vegeta a GOD!`;
-        workoutPTag.innerHTML = ``;
-        agePTag.innerHTML = ``;
-        iframe.src = giphy["god"];
-        vegeta.src = stages[2];
-        videoDiv.innerHTML = videos["god"];
+        span.innerHTML = enemies[2]
+        videoDiv.innerHTML = videos["goku"];
+
+        this.workingOut = 1;
+        this.happy = 1;
+        this.pride = 1;
+
+        workoutProgressBar.value = this.workingOut;
+        happyProgressBar.value = this.happy;
+        prideProgressBar.value = this.pride;
+
+        time = 6000
+        this.isGod = true;
+        setTimeout(() => checkStatus())
     }
     gameIsOver() {
         clearInterval(intervalId)
-        vegeta.src = stages[3];
-        document.body.classList.toggle('dark');
-        hungryPTag.innerHTML = "";
-        happyPTag.innerHTML = "";
-        workoutPTag.innerHTML = "";
-        agePTag.innerHTML = "";
+        document.querySelector('.progress-bar').innerHTML = ''
+        document.querySelector('.content').innerHTML = 'Game Over'
+        soundBoards = null;
+    }
+    winner() {
+        clearInterval(intervalId);
+        document.querySelector('.progress-bar').innerHTML = ''
+        document.querySelector('.content').innerHTML = 'You Won!'
     }
 }
 // --- Init Tamagotchi ---
-
 const tamagotchi = new Vegeta(1, 1, 1, 1, 10, false);
 tamagotchi.default();
 
@@ -141,18 +137,16 @@ function trashTalk() {
     checkStatus();
     audio[Math.floor(Math.random() * 3)].play();
 }
-// const vegetaInsult = () => {
-//     trashTalk();
-//     hungryPTag.innerHTML = `Hunger: - ${tamagotchi.hunger -= 1}`
-//     happyPTag.innerHTML = `Happy: ${tamagotchi.happy -= 1}`;
-//     workoutPTag.innerHTML = `Workout: ${tamagotchi.workingOut -= 1}`;
-//     checkStatus()
-// }
-let intervalId = setInterval(vegetaInsult, time);
-function age() {
-    tamagotchi.aging();
+const vegetaInsult = () => {
+    trashTalk();
+    prideProgressBar.value = tamagotchi.pride -= 1;
+    workoutProgressBar.value = tamagotchi.workingOut -= 1;
+    happyProgressBar.value = tamagotchi.happy -= 1;
+    removeEvent()
+    checkStatus()
+    setTimeout(() => clickFunction(), 5000)
 }
-setInterval(age, 60000);
+let intervalId = setInterval(vegetaInsult, time);
 function bigBangAttack() {
     let audio = new Audio(soundBoards[soundBoards.length - 1]);
     audio.play();
@@ -167,8 +161,8 @@ function trianVegeta() {
     }
 }
 function feedVegeta() {
-    if (tamagotchi.hunger < 10) {
-        tamagotchi.eat();
+    if (tamagotchi.pride < 10) {
+        tamagotchi.sayianPride();
     }
 }
 function happyVegeta() {
@@ -176,57 +170,46 @@ function happyVegeta() {
         tamagotchi.happyPlueOne();
     }
 }
+function attackVegeta() {
+   attackBtn.style.color = '#EFE1CD';
+   attackBtn.style.backgroundColor = '#2955DC';
+   attackBtn.style.cursor = 'pointer';
+}
 function checkStatus() {
-    if (tamagotchi.workingOut === 0 || tamagotchi.happy === 0 || tamagotchi.hunger === 0) {
+    if (tamagotchi.workingOut === 0 || tamagotchi.happy === 0 || tamagotchi.pride === 0) {
         tamagotchi.gameIsOver()
-        changeBtnStyle()
     }
-    if ((!tamagotchi.isSaiyan) && tamagotchi.hunger === 10 && tamagotchi.happy === 10 && tamagotchi.workingOut === 10) {
+    if ((!tamagotchi.isSaiyan) && tamagotchi.pride === 10 && tamagotchi.happy === 10 && tamagotchi.workingOut === 10) {
         console.log('from kid to adult')
         tamagotchi.adultVegeta();
     }
-    if (tamagotchi.isSaiyan && tamagotchi.hunger === 10 && tamagotchi.happy === 10 && tamagotchi.workingOut === 10) {
+    if ((!tamagotchi.isGod) && tamagotchi.isSaiyan && tamagotchi.pride === 10 && tamagotchi.happy === 10 && tamagotchi.workingOut === 10) {
         console.log('adult to god');
-        removeEvent();
         tamagotchi.godVegeta();
+        console.log(tamagotchi);
     }
+    if (tamagotchi.isGod && tamagotchi.isSaiyan && tamagotchi.pride === 10 && tamagotchi.happy === 10 && tamagotchi.workingOut === 10) {
+        console.log('winner');
+        tamagotchi.winner();
+    }
+    if (tamagotchi.pride === 10) {
+        attackVegeta()
+    }
+    console.log('check status');
+
 }
 function removeEvent() {
     workoutBtn.removeEventListener('click', trianVegeta);
-    happyBtn.removeEventListener('click', happyVegeta);
-    eatBtn.removeEventListener('click', feedVegeta)
     workoutBtn.removeEventListener('click', checkStatus);
+    happyBtn.removeEventListener('click', happyVegeta);
     happyBtn.removeEventListener('click', checkStatus);
-    eatBtn.removeEventListener('click', checkStatus)
+    prideBtn.removeEventListener('click', feedVegeta);
+    prideBtn.removeEventListener('click', checkStatus);
 }
-
-// --- Dark Theme ---
-const sleepBtn = document.querySelector('#darkMode');
-sleepBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-});
-// --- Buttons / Event Listners ---
-speakBtn.addEventListener('click', () => {
-    clearInterval(intervalId)
-    setTimeout(() => {
-        intervalId = setInterval(vegetaInsult, time);
-    }, 4000)
-})
 attackBtn.addEventListener('click', () => {
     clearInterval(intervalId)
+    tamagotchi.attack()
     setTimeout(() => {
         intervalId = setInterval(vegetaInsult, time);
-    }, 6000)
+    }, 9000)
 })
-
-// speakBtn.addEventListener('click', princeOfAllSaiyans);
-// attackBtn.addEventListener('click', bigBangAttack);
-// workoutBtn.addEventListener('click', checkStatus);
-// happyBtn.addEventListener('click', happyVegeta);
-// happyBtn.addEventListener('click', checkStatus);
-// eatBtn.addEventListener('click', feedVegeta);
-// eatBtn.addEventListener('click', checkStatus);
-
-function testVegeta() {
-    tamagotchi.thisIsAtest()
-}
